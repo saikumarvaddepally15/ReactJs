d2806e4b16e63cb386860e4b992e967182ca646e
saikumarvaddepally15 <vsai0304@gmail.com>
2023-08-21 19:03:33 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function tick(){
+  const element =  (  <div>
+    <h1>Hello Mr</h1>
+    <h1>The ctime is {new Date().toLocaleTimeString()}</h1>
+  </div> 
+    );
+  root.render(element);
+}
+setInterval(tick,1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
